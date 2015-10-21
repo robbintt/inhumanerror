0003b0241b273920057ed11f781a4c4fd8a26b03
Trent Robbins <robbintt@gmail.com>
2015-10-21 16:04:15 -0700

    Added help text to navigator.

--- navigator.py
+++ navigator.py
@@ -3,6 +3,13 @@ Mock up the navigation aspects of inhumanerror.
 
 Curses supports color, so consider that.
 
+This needs a context manager.
+
+
+
+Options
+=======
+
 Navigation modes: 
     (right arrow) = next passage
     (left arrow) = previous passage
@@ -10,7 +17,6 @@ Navigation modes:
     < = previous index
     i = insert mode
     q = quit and apply changes
-
 
 Characters:
     60 <
@@ -28,64 +34,6 @@ import curses
 
 from curses import KEY_LEFT, KEY_RIGHT
 
-curses.initscr()
-curses.noecho()
-
-win = curses.newwin(20,60,0,0)
-win.keypad(1)
-win.nodelay(1)
-win.border(0)
-
-message1 = "This is the top line."
-message = "This is a message."
-
-index = 0
-mywords = list(range(100))
-
-events = (60, 62, 104, 105, KEY_LEFT, KEY_RIGHT)
-
-event = -1
-while event != 113:
-
-    prev_event = event
-    event = win.getch()
-
-    if event == -1:
-        continue # This handles no new input for the nodelay(1) setting.
-
-    # redraw for ANY user input right now
-    win.clear()
-    win.border(0)
-
-    if event in events:
-
-        # In this design, each function could return a list of strings.
-        # Those strings could be assigned to m1, m2, m3, m4, m5 (five messages)
-        if event == 60:
-            display_index_left()
-        elif event == 62:
-            display_index_right()
-        elif event == 104:
-            display_passage_left()
-        elif event == 105:
-            display_passage_right()
-
-        # This could be done for a list of strings over the index up to a max height.
-        # consider a message truncator for messages that are too long.
-        # you can easily truncate messages to length(=50) with win.addnstr(1,1,message,50)
-        win.addstr(1,1,m1)
-        win.addstr(2,1,m2)
-        win.addstr(3,1,m3)
-        win.addstr(4,1,m4)
-        win.addstr(5,1,m5)
-        win.refresh()
-
-    # let the user know what they pressed.
-    win.addch(18,56,event)
-    #win.addstr(18,50,str(event)) # showes the integer for the event.
-
-curses.endwin()
-
 
 def display_index_left():
     pass
@@ -99,8 +47,90 @@ def display_passage_left():
 def display_passage_right():
     pass
 
+def enter_insert_mode():
+    pass
+
+def display_help():
+    """ the curses view for help 
+    """
+    m1 = "Navigation:"
+    m2 = "'<' - Move left one index"
+    m3 = "'>' - Move right one index"
+    m4 = "left arrow - Move left one passage"
+    m5 = "right arrow - Move right one passage"
+    m6 = "'i' - insert mode, update the word at the current index"
+    m7 = "'h' - This help menu"
+    messages = [m1, m2, m3, m4, m5, m6, m7]
+
+    return messages
+
 
 if __name__ == "__main__":
-    pass
+
+    curses.initscr()
+    curses.noecho()
+
+    win = curses.newwin(20,60,0,0)
+    win.keypad(1)
+    win.nodelay(1)
+    win.border(0)
+    # always display help at bottom.
+    win.addstr(18,1,"Press 'h' for help.")
+
+    # some sample words, not used yet
+    index = 0
+    mywords = list(range(100))
+
+    events = (60, 62, 104, 105, KEY_LEFT, KEY_RIGHT)
+
+    event = -1
+    while event != 113:
+
+        prev_event = event
+        event = win.getch()
+
+        if event == -1:
+            continue # This handles no new input for the nodelay(1) setting.
+
+        # redraw for ANY user input right now
+        win.clear()
+        win.border(0)
+
+        if event in events:
+
+            # In this design, each function could return a list of strings.
+            # all displays are views, except 'i'
+            messages = list()
+            if event == 60:
+                display_index_left()
+            elif event == 62:
+                display_index_right()
+            elif event == KEY_LEFT:
+                display_passage_left()
+            elif event == KEY_RIGHT:
+                display_passage_right()
+            elif event == 104:
+                messages = display_help()
+            elif event == 105:
+                enter_insert_mode()
+
+            # This could be done for a list of strings over the index up to a max height.
+            # consider a message truncator for messages that are too long.
+            # you can easily truncate messages to length(=50) with win.addnstr(1,1,message,50)
+            for i, message in enumerate(messages):
+                if i > 10:
+                    break
+                win.addstr(i,1,message)
+
+        ### THIS IS THE BOTTOM DISPLAY OF THE WINDOW
+        # always display help at bottom.
+        win.addstr(18,1,"Press 'h' for help.")
+        # let the user know what they pressed.
+        win.addch(18,56,event)
+        #win.addstr(18,50,str(event)) # showes the integer for the event.
+        win.refresh()
+
+
+    curses.endwin()
 
 
