e6715711a7c8f7ad020f532053315b87108f53b2
Trent Robbins <robbintt@gmail.com>
2015-10-21 14:10:39 -0700

    Finished input/output module.

--- argparse_boilerplate.py
+++ argparse_boilerplate.py
@@ -12,8 +12,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Provide an input file and an output file.")
     parser.add_argument('-i', type=str, required=True, help="Input file")
     parser.add_argument('-o', type=str, required=True, help="Output file")
-
     args = parser.parse_args()
 
-    print args
+    # User reviews input filename.
+    answer = None
+    while answer not in ('y', 'n', 'q'):
+        answer = raw_input("Is the INPUT filename correct: {} (y/n/q)".format(args.i,))
+    if answer in ('n', 'q'):
+        exit("Aborted.")
 
+    # User reviews output filename.
+    answer = None
+    while answer not in ('y', 'n', 'q'):
+        answer = raw_input("Is the OUTPUT filename correct: {} (y/n/q)".format(args.o,))
+    if answer in ('n', 'q'):
+        exit("Aborted.")
